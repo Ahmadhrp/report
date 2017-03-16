df12f5becea750386ca0088928d1e7b7714b6746
bensolo <aim10hrp@gmail.com>
2017-03-16 17:46:22 +0700

    Report Controller done

--- src/main/java/com/harahap/ibrahim/controller/reportController.java
+++ src/main/java/com/harahap/ibrahim/controller/reportController.java
@@ -12,6 +12,7 @@ import com.harahap.ibrahim.repository.userRepositoryFindByUsernameImpl;
 import java.security.Principal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import static java.util.Arrays.sort;
 import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,45 +23,65 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
  * @author Aim MSI
  */
 @Controller
+@RequestMapping("/report")
 public class reportController {
 
     @Autowired
     private userRepositoryFindByUsernameImpl userRepoFindLoggedInUser;
-    
+
     @Autowired
     private reportRepository reportRepo;
 
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String listReport(Model model) {
+        Sort sort = new Sort(Sort.Direction.ASC, "tanggal");
+        model.addAttribute("reportlist", reportRepo.findAll(sort));
+        return "user/reportlist";
+    }
+
     @RequestMapping(value = "/daily", method = RequestMethod.GET)
-    public String inputReport(Model model) {
-        Sort sort = new Sort( Sort.Direction.ASC, "tanggal" );
-//        List<User> users = new ArrayList<User>();
-//        for( User user : userRepository.findAll( sort ) )
-//        {
-//            users.add( user );
-//        }
-//        return users;
-        model.addAttribute("report", reportRepo.findAll(sort));
-        return "user/formreport";
+    public String inputReport(@RequestParam(value = "id", required = false) Integer id, Model model, Dailyreport report) {
+
+        if (id != null) {
+            if (reportRepo.findOne(id) != null) {
+                report = reportRepo.findOne(id);
+                model.addAttribute("report", report);
+                model.addAttribute("edit", true);
+                return "user/form";
+            }
+        }
+        model.addAttribute("report", new Dailyreport());
+        return "user/form";
+    }
+
+    @RequestMapping("/hapus")
+    public String hapusReport(@RequestParam("id") Integer id) {
+        reportRepo.delete(id);
+        return "redirect:/report/list";
     }
 
     @RequestMapping(value = "/daily", method = RequestMethod.POST)
-    public String postReport(HttpServletRequest request, Principal principal) throws ParseException {
-
+    public String postReport(@RequestParam(value = "id", required = false) Integer id, HttpServletRequest request, Principal principal) throws ParseException {
         SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  
-        Users logged_in_user =  userRepoFindLoggedInUser.findByUsername(user.getUsername()); 
         Date date = dateformat.parse(request.getParameter("tanggal"));
-        //java.sql.Date sql = new java.sql.Date(date.getTime());
-        //System.out.println(logged_in_user.getId());
-        //You can invoke the getter for id on  user object.System.out.println(auth.);
-        //System.out.println("Date dari Form = "+request.getParameter("tanggal"));
-        //System.out.println("Date setelah diparse = "+date);
+
+        if (id != null) {
+            Dailyreport report = reportRepo.findOne(id);
+            report.setTanggal(date);
+            report.setUraian(request.getParameter("uraian"));
+            reportRepo.save(report);
+            return "redirect:/report/list";
+        }
+
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Users logged_in_user = userRepoFindLoggedInUser.findByUsername(user.getUsername());
         Dailyreport report = new Dailyreport();
         report.setUser_id(logged_in_user.getId());
         report.setTanggal(date);
@@ -69,6 +90,6 @@ public class reportController {
         report.setCreatedby(principal.getName());
 
         reportRepo.save(report);
-        return "redirect:daily";
+        return "redirect:/report/list";
     }
 }
